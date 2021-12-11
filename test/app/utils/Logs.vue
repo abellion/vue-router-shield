a4ed66b3dd03179ca114023120bd32d75f54575a
Antoine Bellion <antoinebellion@msn.com>
2021-12-11 15:53:05 +0100

    Add logs renderer

---
+++ test/app/utils/Logs.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col space-y-4">
+    <div class="container flex items-center justify-between px-6 py-4 mx-auto border rounded" v-for="log in logs">
+      <p class="mx-3">{{ log }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as Logger from '../Logger'
+
+export default {
+  data: () => ({
+    logs: Logger.logs
+  })
+}
+</script>
