b4086b2c0a914da48e8f27933ca0e566a0ad5f96
Antoine Bellion <antoinebellion@msn.com>
2021-12-11 13:14:50 +0100

    Draft layout

--- test/app/cases/before-each/components/App.vue
+++ test/app/cases/before-each/components/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <p>
-    Hello World !
-  </p>
+  <div class="bg-white border-b">
+    <div class="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize">
+      <a href="#" class="border-b-2 text-gray-800 border-blue-500 mx-1.5 sm:mx-6">
+        Foo
+      </a>
+
+      <a href="#" class="border-b-2 border-transparent hover:text-gray-800 hover:border-blue-500 mx-1.5 sm:mx-6">
+        Bar
+      </a>
+    </div>
+  </div>
 </template>
