7a9d01d78b4386f00f60f869b09e39f733aaf330
Antoine Bellion <antoinebellion@msn.com>
2021-12-11 15:42:01 +0100

    Add middlewares runner

---
+++ src/Middleware.js
@@ -0,0 +1,55 @@
+class Middleware {
+  constructor (request, next) {
+    this.request = request
+    this.next = next
+  }
+
+  /**
+   * Handle the middlewares
+   * @return mixed
+   */
+  async handle () {
+    const chain = this.chain()
+
+    for (let middleware of chain) {
+      const result = await this.run(middleware)
+
+      if (result !== undefined) {
+        return this.next(result)
+      }
+    }
+
+    return this.next()
+  }
+
+  /**
+   * Run a middleware
+   * @param  function middleware
+   * @return promise
+   */
+  run (middleware) {
+    return new Promise((resolve) => {
+      middleware(this.request.to, this.request.from, resolve)
+    })
+  }
+
+  /**
+   * Build the middleware chain
+   * @return array
+   */
+  chain () {
+    const chain = new Set()
+
+    for (let route of this.request.to.matched) {
+      if (route.meta.hasOwnProperty('middlewares')) {
+        route.meta.middlewares.forEach(Set.prototype.add, chain)
+      }
+    }
+
+    return Array.from(chain)
+  }
+}
+
+export default (to, from, next) => {
+  new Middleware({ to, from }, next).handle()
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import Middleware from './middleware'
+
 export default function install(vue, options) {
   if (Boolean(options) !== true || options.hasOwnProperty('router') !== true) {
     throw new Error('A Vue Router instance must be given to vue-router-middleware')
   }
+
+  options.router.beforeEach(
+    Middleware
+  )
 }
