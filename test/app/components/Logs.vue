0b31e5528bb0206052ad2d415a3d1388c9b462fb
Antoine Bellion <antoinebellion@msn.com>
2022-02-13 14:44:39 +0100

    Add cy target

--- test/app/components/Logs.vue
+++ test/app/components/Logs.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col space-y-4">
-    <div class="container flex items-center justify-between px-6 py-4 mx-auto border rounded" v-for="log in logs">
+  <div class="flex flex-col space-y-4" v-cypress-target="'logs'">
+    <div class="container flex items-center justify-between px-6 py-4 mx-auto border rounded" v-cypress-target="'log'" v-for="log in logs">
       <p class="mx-3">{{ log }}</p>
     </div>
   </div>
