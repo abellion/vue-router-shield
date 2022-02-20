0ffeb700878c887ad5d3e3f12b2c1d917f3ea7ae
Antoine Bellion <antoinebellion@msn.com>
2022-02-20 16:47:43 +0100

    Apply classes on update

--- test/app/Cypress.js
+++ test/app/Cypress.js
@@ -2,6 +2,10 @@ export default function install(vue, options) {
   vue.directive('cypress-target', {
     mounted (el, { value }) {
       el.classList.add(`cy-target-${value}`)
+    },
+
+    updated (el, { value }) {
+      el.classList.add(`cy-target-${value}`)
     }
   })
 }
