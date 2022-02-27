f3a3cdb80cf756679e6db1a3b4ff96a66d98d299
Antoine Bellion <antoinebellion@msn.com>
2022-02-27 16:23:13 +0100

    Naming

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export BeforeUpdate from './Middlewares/BeforeUpdate'
 
 export default function install(vue, options) {
   if (Boolean(options) !== true || options.hasOwnProperty('router') !== true) {
-    throw new Error('A Vue Router instance must be given to vue-router-middleware')
+    throw new Error('A Vue Router instance must be given to vue-router-shield')
   }
 
   options.router.beforeEach(
